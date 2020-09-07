0ac4e56b73e9d83fef426f8b4f17535c37d52ee1
Max <mn27304@gmail.com>
2020-09-08 00:40:46 +0200

    Redux start 105 Ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {createStore} from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state = 0, action)=>{
+
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+        return state -1;
+    case 'RND':
+        return state + action.value   
+    default: 
+      return state;  
+  }
+ 
+
+}
+
+const inc = ()=>{
+  return {
+    type: 'INC'
+  }
+}
+const dec = ()=>{
+  return {
+    type: 'DEC'
+  }
+}
+const rnd = (value)=>{
+  return {
+    type: 'RND',
+    value
+  }
+}
+
+
+const store = createStore(reducer);
+document.getElementById('inc').addEventListener('click', ()=>{
+  store.dispatch(inc());
+});
+document.getElementById('dec').addEventListener('click', ()=>{
+  store.dispatch(dec());
+});
+document.getElementById('rnd').addEventListener('click', ()=>{
+  const value = Math.floor(Math.random() *10);
+  store.dispatch(rnd(value));
+});
+const update = ()=>{
+  document.getElementById('counter').textContent= store.getState()
+}
+
+ store.subscribe(update);
+
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+
+
+
+/* let state = reducer(undefined, {});
+console.log(state);
+state = reducer(undefined, {type: 'INC'});
+console.log(state);
+state = reducer(state, {type: 'INC'});
+console.log(state); */
